9e7b0c68277937d1ebbcd6339048273bb1e39e41
Karunia22 <karuniasariri@gmail.com>
2024-09-07 07:48:24 +0800

    Hari ke-6

---
+++ hari006.java
@@ -0,0 +1,12 @@
+public class komentar {
+    
+    public static void main(String[] args) {
+        // penanda ini untuk komentar untuk satu baris 
+        
+        /*
+        pemanda ini untuk komentar untuk lebih dari satu baris
+        selama berada dalam penanda ini
+         */
+    }
+    
+}
