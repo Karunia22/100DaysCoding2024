323cae59518245bcb628620bc2a5f1b283577af9
Karunia22 <karuniasariri@gmail.com>
2024-09-07 21:05:32 +0800

    hari ke-6

--- hari006.java
+++ hari006.java
@@ -1,12 +1,14 @@
 public class komentar {
     
     public static void main(String[] args) {
-        // penanda ini untuk komentar untuk satu baris 
         
-        /*
-        pemanda ini untuk komentar untuk lebih dari satu baris
-        selama berada dalam penanda ini
-         */
+        System.out.println("hari006");
+
+        //System.out.println("hari ke-6");
+        /* hari ke-6 tidak dapat dicetak karena 
+            dianggap sebagai komentar sehingga tidak dinggap 
+            sebagai program yang harus di proses dan dicetak. 
+            sama halnya dengan kalimat ini*/
     }
     
 }
