a73a8d442a01dc5f31ec27809d50baa1032d4797
Karunia22 <karuniasariri@gmail.com>
2024-09-08 19:37:25 +0800

    Hari ke-7

---
+++ hari007.java
@@ -0,0 +1,12 @@
+public class inisialisasi {
+    
+    public static void main(String[] args) {
+        
+        int nilai1 = 12; // inisialisasi nilai variabel nilai1 = 12
+        float nilai2 = 12.5f; // inisialisasi nilai variabel nilai2 = 12.5
+        
+        System.out.println(nilai1);
+        System.out.println(nilai2);
+    }
+    
+}
