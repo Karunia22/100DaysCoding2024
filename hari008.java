15fc7fe460b41db48d9a2b80b0c513f412d371e3
Karunia22 <karuniasariri@gmail.com>
2024-09-09 21:53:12 +0800

    Hari ke-8

---
+++ hari008.java
@@ -0,0 +1,12 @@
+public class update {
+    
+    public static void main(String[] args) {
+        int nilaiAwal = 0;
+        
+        //meng-update nilai variabel
+        nilaiAwal = 10;
+        
+        System.out.println(nilaiAwal);
+    }
+    
+}
