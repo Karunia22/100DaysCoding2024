bf7974b8fb05854838dfc8806a6aae7209d68714
Karunia22 <karuniasariri@gmail.com>
2024-09-11 20:33:28 +0800

    Hari ke-10

---
+++ hari010.java
@@ -0,0 +1,13 @@
+public class tipeDataByte {
+    
+    public static void main(String[] args) {
+        
+        //tipe data byte 8 bit atau 1 byte memiliki range -128 sampai 127 
+        
+        byte a = 12;
+        byte b = 12;
+        
+        System.out.println(a+b);
+    }
+    
+}
