b9bea0e88820b48b07dc17c34ba9bc43a760ad36
Karunia22 <karuniasariri@gmail.com>
2024-09-12 23:13:18 +0800

    Hari ke-11

---
+++ hari011.java
@@ -0,0 +1,12 @@
+public class tipeDataShort {
+    
+    public static void main(String[] args) {
+        
+        short a = 12;
+        short b = 12;
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
