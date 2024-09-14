8c586e3f1da12a9f7bb7d41aca91347260ded7b6
Karunia22 <karuniasariri@gmail.com>
2024-09-14 20:56:54 +0800

    Hari ke-13

---
+++ hari013.java
@@ -0,0 +1,18 @@
+public class MenukarNilaiVariabel {
+    
+    public static void main(String[] args) {
+        
+        int a = 120;
+        int b = 15;
+        
+        int c;
+        
+        c = a;
+        a = b;
+        b = c;
+        
+        System.out.println("nilai A setelah diubah : "+ a);
+        System.out.println("nilai B setelah diubah : " +b);
+    }
+    
+}
