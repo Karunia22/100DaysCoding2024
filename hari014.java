0a5166b9cb80b39c1f6e506d2d39e964570b8afc
Karunia22 <karuniasariri@gmail.com>
2024-09-15 22:37:08 +0800

    Hari ke-14

--- hari014.java
+++ hari014.java
@@ -1,7 +1,7 @@
 public class TipeDataLong {
     
     public static void main(String[] args) {
-        long r = 100;
+        long r = 9223372036854775807l;
         
         System.out.println(r);
     }
