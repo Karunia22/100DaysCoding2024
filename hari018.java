4d6bd5f59cb4ed6e54fbb2a18c1296c0ac7cec50
Karunia22 <karuniasariri@gmail.com>
2024-09-19 23:09:20 +0800

    Hari ke-18

---
+++ hari018.java
@@ -0,0 +1,11 @@
+public class TipeDataBoolean {
+    
+    public static void main(String[] args) {
+        boolean b = true;
+        boolean s = false;
+        
+        System.out.println(b);
+        System.out.println(s);
+    }
+    
+}
