f643c5f04f1a914b798e97931ea16a05fde0f705
Karunia22 <karuniasariri@gmail.com>
2024-09-21 22:54:51 +0800

    Hari ke-20

---
+++ hari020.java
@@ -0,0 +1,12 @@
+public class TipeDataReferenceInteger {
+    
+    public static void main(String[] args) {
+        Integer angka = new Integer(3);
+        String angka2 = Integer.toString(angka);
+        int angka3 = angka.intValue();
+        
+        System.out.println(angka);
+        System.out.println(angka2);
+        System.out.println(angka3);
+    }
+}
