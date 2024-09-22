c017e4d89313eafe8b55ac0de8baedb04e430a03
Karunia22 <karuniasariri@gmail.com>
2024-09-22 22:10:05 +0800

    Hari ke-21

---
+++ hari021.java
@@ -0,0 +1,10 @@
+public class TipeDataReferenceShort {
+    
+    public static void main(String[] args) {
+        Short angka2 = 5;
+        
+        System.out.println(angka2.longValue());
+        System.out.println(angka2.shortValue());
+        
+    }
+}
