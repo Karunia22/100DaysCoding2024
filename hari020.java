b66a875fe126f516dc4551a6242b4a29b4910d68
Karunia22 <karuniasariri@gmail.com>
2024-09-22 22:07:33 +0800

    Hari ke-20

--- hari020.java
+++ hari020.java
@@ -1,12 +1,10 @@
-public class TipeDataReferenceInteger {
+public class TipeDataReferenceByte {
     
     public static void main(String[] args) {
-        Integer angka = 3;
-        String angka2 = Integer.toString(angka);
-        int angka3 = angka.intValue();
+        Byte angka1 = 2;
         
-        System.out.println(angka);
-        System.out.println(angka2);
-        System.out.println(angka3);
+        System.out.println(angka1.byteValue());
+        System.out.println(angka1.intValue());
+        
     }
 }
