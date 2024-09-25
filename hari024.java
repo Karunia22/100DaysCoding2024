8b11edaf9a0026382a3c5433a6d35d12118cd08f
Karunia22 <karuniasariri@gmail.com>
2024-09-25 20:32:44 +0800

    Hari ke-24

---
+++ hari024.java
@@ -0,0 +1,9 @@
+public class TipeDataReferenceFloat {
+    
+    public static void main(String[] args) {
+        Float k = 23.542f;
+        Float p = 23.42f;
+        System.out.println(k.floatValue());
+        System.out.println(k.equals(p));
+    }
+}
