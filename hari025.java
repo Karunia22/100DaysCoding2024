961389d3c7905aad0d07a4be72a9d947f0dcfa0e
Karunia22 <karuniasariri@gmail.com>
2024-09-26 23:26:29 +0800

    Hari ke-25

---
+++ hari025.java
@@ -0,0 +1,8 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        Double k = 2.467;
+        System.out.println(k.doubleValue());
+        System.out.println(k.floatValue());
+    }
+}
