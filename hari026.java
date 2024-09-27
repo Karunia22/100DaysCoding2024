0893d9c58430885e1497d0244ed025c6bf3bb81c
Karunia22 <karuniasariri@gmail.com>
2024-09-27 22:33:21 +0800

    Hari ke-26

---
+++ hari026.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        Character k = 'K';
+        String b = k.toString();
+        char a = k.charValue();
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
