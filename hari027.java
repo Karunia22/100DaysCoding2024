f1c55dd3565728a9abb566ee4ee59569385ed674
Karunia22 <karuniasariri@gmail.com>
2024-09-28 22:58:14 +0800

    Hari ke-27

---
+++ hari027.java
@@ -0,0 +1,8 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        String kalimat = "rata-rata tinggi orang indonesia adalah";
+        double tinggi = 158.17;
+        System.out.printf("%s %f", kalimat, tinggi);
+    }
+}
