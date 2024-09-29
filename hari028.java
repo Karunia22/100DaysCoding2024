d91efc92b110fcc3ed95e3209d9bd2cc24c3c403
Karunia22 <karuniasariri@gmail.com>
2024-09-29 23:24:21 +0800

    Hari ke-28

---
+++ hari028.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        Boolean t = true;
+        Boolean k = false;
+        
+        System.out.println(t.booleanValue());
+        System.out.println(Boolean.toString(k));
+    }
+} 
