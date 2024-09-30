1070f72fbbd38a14e794e2e58ac43eabdd67c8a2
Karunia22 <karuniasariri@gmail.com>
2024-09-30 22:41:30 +0800

    Hari ke-29

---
+++ hari029.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+public class Main {
+    
+    public static void main(String[] args) {
+        BigInteger bil1 = new BigInteger("10000000000000052691872627293927179492828396");
+        BigInteger bil2 = new BigInteger("25263738287363663636362728474728286463727162");
+        
+        System.out.println(bil1.add(bil2)); // penjumlahan
+        System.out.println(bil1.subtract(bil2)); //pengurangan
+        System.out.println(bil1.multiply(bil2)); //perkalian
+        System.out.println(bil1.divide(bil2)); //pembagian
+        System.out.println(bil1.mod(bil2));
+    }
+} 
