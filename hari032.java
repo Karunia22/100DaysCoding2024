3603c3413ff29c2ff5c97ff2ef9339ce4e559e65
Karunia22 <karuniasariri@gmail.com>
2024-10-03 23:21:16 +0800

    Hari ke-32

---
+++ hari032.java
@@ -0,0 +1,31 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        // Bilangan bulat
+        int bil1 = 10;
+        byte bil2 = 21;
+        short bil3 = 2;
+        long bil4 = 1929288292l;
+        
+        // bilangan desimal atau bilangan pecahan
+        float bil5 = 3.4f;
+        double bil6 = 5.55;
+        
+        // karakter
+        char kar = 'a';
+        
+        // logika
+        boolean log = true;
+        
+        System.out.println(Integer.toString(bil1));
+        System.out.println(Byte.toString(bil2));
+        System.out.println(Short.toString(bil3));
+        System.out.println(Long.toString(bil4));
+        System.out.println(Float.toString(bil5));
+        System.out.println(Double.toString(bil6));
+        System.out.println(Character.toString(kar));
+        System.out.println(Boolean.toString(log));
+    }
+    
+}
