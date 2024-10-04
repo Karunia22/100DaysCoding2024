077fa7ce804a80417686f1bd008cd4b7ca6bb0ca
Karunia22 <karuniasariri@gmail.com>
2024-10-04 22:31:38 +0800

    Hari ke-33

---
+++ hari033.java
@@ -0,0 +1,16 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        //casting otomatis
+        byte bil = 13;
+        int bilCasting = bil;
+        
+        //casting manual
+        float bil2 = 3.5f;
+        double bil2Casting = (double) bil2;
+            
+        System.out.println(bilCasting);
+        System.out.println(bil2Casting);
+    }
+    
+}
