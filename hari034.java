818cf181a8d3e6314fcc5670d4d02fcf40f9d7c3
Karunia22 <karuniasariri@gmail.com>
2024-10-05 23:15:16 +0800

    Hari ke-34

---
+++ hari034.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String kalimat = input.nextLine();
+        int bilangan = input.nextInt();
+        
+        System.out.println(kalimat);
+        System.out.println(bilangan);
+    }
+    
+}
