2d70d43d7bc4938fb36e2318420461c7a1bc0666
Karunia22 <karuniasariri@gmail.com>
2024-10-12 15:35:29 +0800

    Hari ke-41b

---
+++ hari041b.java
@@ -0,0 +1,19 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 20;
+        
+        System.out.println(b);
+        b%=a;
+        System.out.println(b);
+        b*=a;
+        System.out.println(b);
+        b-=a;
+        System.out.println(b);
+        b/=a;
+        System.out.println(b);
+        b+=a;
+        System.out.println(b);
+    }
+} 
