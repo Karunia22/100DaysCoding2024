8e0a30c708b92fb10a16c5a7edd8a87fd5ed2e27
Karunia22 <karuniasariri@gmail.com>
2024-11-01 22:51:46 +0800

    hari ke-61

---
+++ hari061.java
@@ -0,0 +1,12 @@
+public class hari061 {
+        public static void main(String[]args){
+                int k = 0;
+                int p = 0;
+                int l = 0;
+
+                while(true){
+                        System.out.println(k);
+                        k++;
+                };
+        }
+}
