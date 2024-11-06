0eae1cfddf07eaaea667db792cbb69c9c540a53e
Karunia22 <karuniasariri@gmail.com>
2024-11-06 21:58:08 +0800

    Hari ke-66

---
+++ hari066.java
@@ -0,0 +1,24 @@
+public class hari066 {
+        public static void main(String[] args){
+                System.out.println("Persegi panjang");
+                for(int i = 1; i <= 5; i++){
+                        for(int j = 1; j <= 10; j++){ 
+                                System.out.print("* ");
+                        }
+                System.out.println();
+                }
+
+                System.out.println("\nPersegi");
+                int i = 1;
+                while (i<=5){
+                int j = 1;
+                        while(j<=5){
+                        System.out.print("* ");
+                        j++;
+                        }
+                System.out.println();
+                i++;
+                }
+        }
+}
+
